7a6fa293e2245e2493e77bf2074437211e2c48b7
null <null>
2026-08-14 23:33:29 +0300

    git add src/components/ScrollToTop.jsx, src/components/SiteLayout.jsx git commit -m "scrolltotop and layout details" git push

--- src/components/SiteLayout.jsx
+++ src/components/SiteLayout.jsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import PageArrows from "./PageArrows";
+import ScrollToTop from "./ScrollToTop.jsx";
 
 function SiteLayout() {
   return (
     <>
+      <ScrollToTop />
+      
       <Header />
 
       <Outlet />
